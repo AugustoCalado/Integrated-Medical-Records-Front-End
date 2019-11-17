3f0367f78bf5b97aff1600e632ce7ac59d5873b8
Regiany Almeida <regiany.nunes@ibm.com>
2019-11-17 20:06:29 -0300

    Ajustes de CSS nos componentes de vacinação e ocorrencias

--- src/app/ocorrencias/ocorrencias.component.ts
+++ src/app/ocorrencias/ocorrencias.component.ts
@@ -27,8 +27,19 @@ export class OcorrenciasComponent implements OnInit {
       console.log("---- dentro do response ---- ")
       console.log(response)
       let occ = response.map(current =>{
-        console.log(current.medicalRecordDate)
         let newDateFormat = current.medicalRecordDate.split("::")
+        if(newDateFormat[2] == "1") newDateFormat[2] = "Janeiro"
+        if(newDateFormat[2] == "2") newDateFormat[2] = "Fevereiro"
+        if(newDateFormat[2] == "3") newDateFormat[2] = "Março"
+        if(newDateFormat[2] == "4") newDateFormat[2] = "Abril"
+        if(newDateFormat[2] == "5") newDateFormat[2] = "Maio"
+        if(newDateFormat[2] == "6") newDateFormat[2] = "Junho"
+        if(newDateFormat[2] == "7") newDateFormat[2] = "Julho"
+        if(newDateFormat[2] == "8") newDateFormat[2] = "Agosto"
+        if(newDateFormat[2] == "9") newDateFormat[2] = "Setembro"
+        if(newDateFormat[2] == "10") newDateFormat[2] = "Outubro"
+        if(newDateFormat[2] == "11") newDateFormat[2] = "Novembro"
+        if(newDateFormat[2] == "12") newDateFormat[2] = "Dezembro"
         current.medicalRecordDate = [newDateFormat[0],newDateFormat[1],newDateFormat[2]]
         return current
       })
